651c6f25921cc23eb0fe57951afc4d6efce5ddfb
natdagr85 <nathanmansoor1128@gmail.com>
2023-01-22 13:44:13 -0800

    added image rendering to all catagory pages

--- src/components/Breakfast.jsx
+++ src/components/Breakfast.jsx
@@ -15,12 +15,29 @@ function Breakfast () {
 
   if (!post) return null;
 
-    return (
-        <div>
-            <h2>this is the breakfast page</h2>
-            <img src={post.meals[0].strMealThumb}></img>
-        </div>
-    )
-}
+  let mealArr = []
+  let imgArr = []
+ 
+  for (let i = 0; i < post.meals.length; i++){
+   Object.entries(post.meals[i]).forEach(field => {
+     if(field[1]){
+         if (field[0].startsWith('idMeal')){
+           mealArr.push(field[1])
+         } else if (field[0].startsWith('strMealThumb')){
+             imgArr.push(field[1])
+     }
+  }})
+ }
+  console.log(mealArr, imgArr)
+ 
+     return (
+         <div>
+             <h2>this is the breakfast page</h2>
+             <ul>
+                 {imgArr.map((item, index) => <li key={index}><button className="imgbutton"><img src={item} onClick={() =>{}}></img></button></li>)}
+             </ul>
+         </div>
+     )
+ }
 
 export default Breakfast
--- src/components/Seafood.jsx
+++ src/components/Seafood.jsx
@@ -15,12 +15,28 @@ function Seafood () {
 
   if (!post) return null;
 
+  let mealArr = []
+ let imgArr = []
+
+ for (let i = 0; i < post.meals.length; i++){
+  Object.entries(post.meals[i]).forEach(field => {
+    if(field[1]){
+        if (field[0].startsWith('idMeal')){
+          mealArr.push(field[1])
+        } else if (field[0].startsWith('strMealThumb')){
+            imgArr.push(field[1])
+    }
+ }})
+}
+ console.log(mealArr, imgArr)
+
     return (
         <div>
-            <h2>this is the Seafood page</h2>
-            <img src={post.meals[0].strMealThumb}></img>
+            <h2>this is the seafood page</h2>
+            <ul>
+                {imgArr.map((item, index) => <li key={index}><button className="imgbutton"><img src={item} onClick={() =>{}}></img></button></li>)}
+            </ul>
         </div>
     )
 }
-
 export default Seafood
