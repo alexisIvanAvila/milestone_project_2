f972cd64489059d896ba51cf9d0c8faa76d0fbba
natdagr85 <nathanmansoor1128@gmail.com>
2023-01-28 17:21:32 -0800

    testing DB deployment with favorites

--- server/server.js
+++ server/server.js
@@ -75,7 +75,7 @@ app.post('/api/favorites', async (req, res) => {
     let result = await client.query(query);
     result = result.rows
     console.log(result)
-    let query2 = `INSERT INTO favorites (token, id, category) values('${token}', ${result[0]['id']}, ${category})`;
+    let query2 = `INSERT INTO favorites (token, id, category) values('${token}', ${result[0]['id']}, category)`;
     let result2 = await client.query(query2);
     return res.status(200).json(result)
 })
@@ -91,14 +91,14 @@ app.delete('/api/favorites', async (req, res) => {
     // if (result.length === 0) {
     //     return res.status(200)
     // }
-    result = result.map((res) => res['id'])
+    // result = result.map((res) => res['id'])
     // let query2 = `SELECT * FROM favorites WHERE token='${token}' and id=` + result.join(' or id=')
     // let result2 = await client.query(query2)
     // result2 = result2.rows
     // if (result2.length === 0) {
     //     return res.status(200)
     // }
-    result2 = result2.map((res) => res['id'])
+    // result2 = result2.map((res) => res['id'])
     // let query3 = `DELETE FROM favorites WHERE token='${token}' and id=` + result2[0]
     // let result3 = await client.query(query3)
     // let query4 = `DELETE FROM ${category} WHERE id=` + result2[0]
